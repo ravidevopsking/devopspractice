e9fb54d8d1644a8fdfec7299e37c2c24d9ac7fde
ravidevopsking <ravitejakavya.mit@gmail.com>
2023-09-06 16:29:12 +0530

    BACKEND services added

---
+++ learn-shell/EXPENSE PROJECT/backend.sh
@@ -0,0 +1,14 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+dnf install nodejs -y
+useradd expense
+mkdir /app
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+cp backend.service /etc/systemd/system/backend.service
+cd /app
+unzip /tmp/backend.zip
+npm install
+systemctl daemon-reload
+systemctl enable backend
+systemctl start backend
+dnf install mysql -y
+mysql -h 172.31.46.91 -uroot -pExpenseApp@1 < /app/schema/backend.sql
