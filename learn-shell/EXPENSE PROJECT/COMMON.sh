686fd33b987e43cda271b994357b0f008182de23
ravidevopsking <ravitejakavya.mit@gmail.com>
2023-09-08 13:04:17 +0530

    common file added.

--- learn-shell/EXPENSE PROJECT/COMMON.sh
+++ learn-shell/EXPENSE PROJECT/COMMON.sh
@@ -1,5 +1,8 @@
-log_file=/tmp/expense.log
+#Note: This file content will be imported to other files as source common.sh
+log_file=/tmp/expense.log     #all log files aare stored to /tmp/expense.log
 
+
+#creating common function, this function will import to other files and access by calling download_and_extract
 download_and_extract() {
   echo Download $component Code
   curl -s -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip >>$log_file
