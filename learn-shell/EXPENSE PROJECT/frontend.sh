185d304b134f3db94528ba11de385ae4f9f60242
ravidevopsking <ravitejakavya.mit@gmail.com>
2023-09-08 13:23:26 +0530

    frontend file updated with importing common.sh and component as frontend

--- learn-shell/EXPENSE PROJECT/frontend.sh
+++ learn-shell/EXPENSE PROJECT/frontend.sh
@@ -1,9 +1,19 @@
-dnf install nginx -y
-systemctl enable nginx
-systemctl start nginx
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
-cp expense.conf /etc/nginx/default.d/expense.conf
+source COMMON.sh
+component=frontend
+
+echo Installing nginx
+dnf install nginx -y &>>$log_file
+
+echo removing old nginx content
+rm -rf /usr/share/nginx/html/* &>>$log_file
+
+echo keeping expense config file in nginx
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
+
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-systemctl restart nginx
+
+download_and_extract
+
+echo starting nginx server
+systemctl enable nginx &>>$log_file
+systemctl restart nginx &>>$log_file
