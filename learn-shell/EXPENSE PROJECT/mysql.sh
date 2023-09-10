d0a26006563c6c082d00da3e82ad23f894262039
ravidevopsking <ravitejakavya.mit@gmail.com>
2023-09-10 15:44:46 +0530

    stat checks,password from argument updated

--- learn-shell/EXPENSE PROJECT/mysql.sh
+++ learn-shell/EXPENSE PROJECT/mysql.sh
@@ -1,6 +1,23 @@
-dnf module disable mysql -y
-cp mysql.repo /etc/yum.repos.d/mysql.repo
-dnf install mysql-community-server -y
-systemctl enable mysqld
-systemctl start mysqld
-mysql_secure_installation --set-root-pass ExpenseApp@1
+source common.sh
+
+echo Disable MySQL 8 Version
+dnf module disable mysql -y &>>$log_file
+stat_check
+
+echo Copy MySQL Repo file
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
+stat_check
+
+echo Install MySQL Server
+dnf install mysql-community-server -y &>>$log_file
+stat_check
+
+echo Start MySQL Service
+systemctl enable mysqld &>>$log_file
+systemctl start mysqld &>>$log_file
+stat_check
+
+echo Setup root Password
+mysql_root_password=$1
+mysql_secure_installation --set-root-pass $mysql_root_password &>>$log_file
+stat_check
