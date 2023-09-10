92732d6115d976f6521372976c94a77f76a738d5
ravidevopsking <ravitejakavya.mit@gmail.com>
2023-09-10 14:13:31 +0530

    status_check function added

--- learn-shell/EXPENSE PROJECT/COMMON.sh
+++ learn-shell/EXPENSE PROJECT/COMMON.sh
@@ -6,9 +6,18 @@ log_file=/tmp/expense.log     #all log files aare stored to /tmp/expense.log
 download_and_extract() {
   echo Download $component Code
   curl -s -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip >>$log_file
-  echo $?
+  stat_check   #calling stat_check function to check above status of command
 
   echo Extracting $component Code
   unzip /tmp/$component.zip >>$log_file
-  echo $?
+  stat_check
+}
+#creating status check function
+stat_check() {
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILED\e[0m"
+    exit 1
+  fi
 }
